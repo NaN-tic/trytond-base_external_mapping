9cec8342c6e4da1c4e69c549432a7863c841cf6b
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:08:38 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# This file is part base_external_mapping module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-try:
-    from trytond.modules.base_external_mapping.tests.test_suite import suite
-except ImportError:
-    from .test_base_external_mapping import suite
-
-__all__ = ['suite']
